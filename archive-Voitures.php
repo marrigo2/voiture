543829573d4a3cbd8d70b1fb5fefe857476df7a7
Maxime Arrigoni <maxime.arrigoni@edu.univ-fcomte.fr>
2020-10-02 09:34:28 +0200

    Marque voiture correction

--- archive-Voitures.php
+++ archive-Voitures.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
-<div class="salles">
+<div class="Voitures">
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
-      <article class="projet">
-        <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br> 
+      <article class="voiture">
         <h1 class="title">
           <a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
           </a>
         </h1>
+
         <?php the_post_thumbnail( 'thumbnail' ); ?>
+        <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br> 
+
         <div class="content">
           <?php the_content(); ?>
         </div>
--- taxonomy-marque.php
+++ taxonomy-marque.php
@@ -4,13 +4,14 @@
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <article class="projet">
-        <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br> 
         <h1 class="title">
           <a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
           </a>
         </h1>
         <?php the_post_thumbnail( 'thumbnail' ); ?>
+        <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br> 
+
         <div class="content">
           <?php the_content(); ?>
         </div>
